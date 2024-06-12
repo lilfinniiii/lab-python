f222d60e19f34266fa4b469af19a67ce177d2fb8
lilfinniiii <145673745+lilfinniiii@users.noreply.github.com>
2024-06-12 14:20:47 +0300

    Add files via upload

---
+++ lab2/main.py
@@ -0,0 +1,23 @@
+def task1(number1,znak,number2):
+    if znak == ">":
+        return number1 > number2
+    if znak == "<":
+        return number1 > number2
+    if znak == ">=":
+        return number1 > number2
+    if znak == "<=":
+        return number1 > number2
+    if znak == "==":
+        return number1 > number2
+    if znak == "!=":
+        return number1 > number2
+
+def task2(text, number):
+    if len(text) > number:
+        return(len(text))
+    else:
+        return(number)
+
+def task3(number1,number2,number3):
+    return number1 == number2 == number3
+
